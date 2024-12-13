65abb23e9f392af285422964b4130dbe316094e1
257m <107591653+257m@users.noreply.github.com>
2024-12-13 20:09:00 +0000

    Update timeElapsed.m

--- timeElapsed.m
+++ timeElapsed.m
@@ -29,3 +29,55 @@ function newArray = timeElapsed(datetime_array)
     % the array at 0.
         newArray = newArray - first;  
 end
+
+% Load our data and setup up some variables from the data
+clear
+load('ExampleData.mat');
+lat=Position.latitude;
+lon=Position.longitude;
+positionDatetime=Position.Timestamp;
+Xacc = Acceleration.X;
+Yacc = Acceleration.Y;
+Zacc = Acceleration.Z;
+accelDatetime=Acceleration.Timestamp;
+% We use the following to obtain linear time data in seconds from a datetime array
+positionTime=timeElapsed(positionDatetime);
+accelTime=timeElapsed(accelDatetime);
+
+earthCirc = 24901;
+totaldis = 0;
+% Set stride as constant variable
+stride = 2.5; % Average stride (ft)
+
+distanceArray = [];
+% Loop over the data and sum up the distance
+for i = 1:(length(lat)-1)
+    lat1 = lat(i); % The first latitude
+    lat2 = lat(i+1); % The second latitude
+    lon1 = lon(i); % The first longitude
+    lon2 = lon(i+1); % The second longitude
+
+    degDis = distance(lat1, lon1, lat2, lon2);
+    dis = (degDis/360)*earthCirc;
+    distanceArray(i) = totaldis;
+
+    totaldis = totaldis + dis;
+end
+
+strides = distanceArray / stride;
+
+% Calculate steps based off stride
+totaldis_ft = totaldis*5280; % Converting distance from miles to feet
+steps = totaldis_ft/stride;
+
+% Print out our distance and steps to the command window
+disp(['The total distance traveled is: ', num2str(totaldis),' miles'])
+disp(['You took ', num2str(steps) ' steps'])
+
+% Create a plot with our distance and steps
+plot(distanceArray);
+legend('Distance');
+xlabel('Time (s)')
+ylabel('Steps and Distance');
+title('Distance Data Vs. Time');
+hold off
